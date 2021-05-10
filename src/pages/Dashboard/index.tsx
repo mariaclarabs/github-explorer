cce74c7bed55f47cb09136ef52063974b460fe88
mariaclarabs <clarabsouza@hotmail.com>
2021-05-09 21:51:53 -0300

    Conectando github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,62 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const res = await api.get<Repository>(`repos/${newRepo}`);
+
+    setRepositories([...repositories, res.data]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github.</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="#teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/39465910?v=4"
-            alt="userImg"
-          />
-          <div>
-            <strong>mariaclarabs/go-barber</strong>
-            <p>No description, website, or topics provided.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/39465910?v=4"
-            alt="userImg"
-          />
-          <div>
-            <strong>mariaclarabs/go-barber</strong>
-            <p>No description, website, or topics provided.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/39465910?v=4"
-            alt="userImg"
-          />
-          <div>
-            <strong>mariaclarabs/go-barber</strong>
-            <p>No description, website, or topics provided.</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
