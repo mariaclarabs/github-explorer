e9f843e25faa69c6e7c568484826589d9b97613a
mariaclarabs <clarabsouza@hotmail.com>
2021-05-10 21:58:59 -0300

    Styling repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,61 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { /* useRouteMatch, */ Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-interface RepositoryParams {
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+/* interface RepositoryParams {
   repository: string;
-}
+} */
 
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>();
+  // const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="User" />
+          <div>
+            <strong>Repo</strong>
+            <p>Desc</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>55</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>85</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link key="key" to="redirect">
+          <div>
+            <strong>Issue1</strong>
+            <p>Blablabla</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
